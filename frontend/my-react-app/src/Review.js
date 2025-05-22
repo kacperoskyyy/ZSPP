c030639374cf40cdc25561ed89107e143a5a5478
BartekRu <148788567+BartekRu@users.noreply.github.com>
2025-05-22 12:09:32 +0200

    Update 22.05
    Review

--- frontend/my-react-app/src/Review.js
+++ frontend/my-react-app/src/Review.js
@@ -74,35 +74,12 @@ import Karuzela from "./Carousel";
         <div className="arrow-up"></div>
       </div>
       <div className="review-page">
-     <Karuzela reviews={Reviews} slideWidth={330} interval={3000} /> 
+     <Karuzela reviews={Reviews} slideWidth={300} interval={3000} /> 
   </div>
   </div>
 );
 
-      {/* <section className="review-section">
-        <h1>Opinie naszych klientow</h1>
 
-        <div className="review-container">
-          {Reviews.map((review) => (
-            <div key={review.id} className="review-card">
-              <div className="review-header">
-                <div className="review-avatar"></div>
-                <h3>{review.name}</h3>
-              </div>
-              <p>{review.text}</p>
-              <div className="rating">
-                {[...Array(review.rating)].map((_, i) => (
-                  <span key={i} className="star">
-                    #
-                  </span>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-    </div> */}
- 
 };
 
 export default ReviewSection;
