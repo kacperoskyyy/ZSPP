c7d3d9332fe06d4a3eebfae35f4cbf4bf9ef6cfd
BartekRu <148788567+BartekRu@users.noreply.github.com>
2025-06-10 21:54:19 +0200

    Poprawka Log Reg

--- frontend/my-react-app/src/components/FormContainer.js
+++ frontend/my-react-app/src/components/FormContainer.js
@@ -1,20 +1,50 @@
 
+// import React from 'react';
+// import './FormContainer.css';
+
+// const FormContainer = ({ 
+//   children, 
+//   className = '', 
+//   theme = 'default',
+//   size = 'default',
+//   customStyles = {} 
+// }) => {
+//   const containerClasses = `form-container-wrapper ${theme !== 'default' ? `theme-${theme}` : ''} ${className}`;
+//   const formClasses = `form-container-inner ${size !== 'default' ? `size-${size}` : ''}`;
+
+//   return (
+//     <div className={containerClasses} style={customStyles.container}>
+//       <div className={formClasses} style={customStyles.form}>
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FormContainer;
+
 import React from 'react';
 import './FormContainer.css';
 
-const FormContainer = ({ 
-  children, 
-  className = '', 
+const FormContainer = ({
+  children,
+  className = '',
   theme = 'default',
   size = 'default',
-  customStyles = {} 
+  customStyles = {}
 }) => {
   const containerClasses = `form-container-wrapper ${theme !== 'default' ? `theme-${theme}` : ''} ${className}`;
   const formClasses = `form-container-inner ${size !== 'default' ? `size-${size}` : ''}`;
 
   return (
-    <div className={containerClasses} style={customStyles.container}>
-      <div className={formClasses} style={customStyles.form}>
+    <div
+      className={containerClasses}
+      style={customStyles.container}
+    >
+      <div
+        className={formClasses}
+        style={customStyles.form}
+      >
         {children}
       </div>
     </div>
