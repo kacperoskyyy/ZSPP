96b7d9181605aa5b8d63f518267103dceb81e826
s0s3nk4 <msosna2929@gmail.com>
2025-06-11 22:44:05 +0200

    poprawka styli

--- frontend/my-react-app/src/pages/admin/ManagePayment.jsx
+++ frontend/my-react-app/src/pages/admin/ManagePayment.jsx
@@ -19,13 +19,15 @@ const ManagePayment = () => {
   if (!reservation) return <p>Ładowanie...</p>;
 
   return (
-    <div style={{ padding: 20 }}>
+    <div style={{ padding: 20 }} className="manage-payment-container">
       <h2>Podsumowanie rezerwacji #{reservation.id}</h2>
       <p><strong>Użytkownik:</strong> {reservation.user_id}</p>
       <p><strong>Okres:</strong> {new Date(reservation.start_date).toLocaleDateString()} - {new Date(reservation.end_date).toLocaleDateString()}</p>
       <p><strong>Status:</strong> {reservation.status}</p>
-      {/* tutaj możesz dodać opcję "zatwierdzenia płatności" */}
-      <button onClick={() => alert("Płatność zatwierdzona!")}>Zatwierdź płatność</button>
+
+      <button onClick={() => alert("Płatność zatwierdzona!")/* TUTAJ POWINNA BYC FUNKCJA, KTORA ODPYTUJE API ALE NW 
+    CZY JEST NA TO ENDPOINT ZROBIONY */}>Zatwierdź płatność</button>
+      <button onClick={onBack} style={{ marginLeft: 10 }}>Powrót</button>
     </div>
   );
 };
